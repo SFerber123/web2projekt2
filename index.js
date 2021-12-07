c52ee578ec2806f5be0268065e243e292bb208b5
Stjepan Ferber <sf49671@fer.hr>
2021-12-07 17:22:35 +0100

    peto

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const express = require('express');
 const bodyparser = require('body-parser');
+const fs=require('fs');
 var app = express();
 //Configuring express server
 app.use(bodyparser.json());
@@ -17,7 +18,21 @@ var mysqlConnection = mysql.createConnection({
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
 
-
+app.get('/',(req,res)=>{
+    
+    res.writeHead(200,{
+        'Content-Type':'text/html'
+        });
+    fs.readFile('./index.html',null,function (error, data) {
+        if (error) {
+            res.writeHead(404);
+            res.write('Whoops! File not found!');
+        } else {
+            res.write(data);
+        }
+        res.end();
+        });
+    })
 app.get('/learners/:id' , (req, res) => {
     mysqlConnection.connect((err)=> {
         if(!err)
